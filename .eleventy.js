a9fdfc847af96a446fd08a2b07f8fbfb03394d07
Phil Parsons <p-m-p@users.noreply.github.com>
2024-10-02 07:37:33 +0100

    feat: add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,26 @@
-export default function (eleventyConfig) {
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addBundle("css");
+
+  eleventyConfig.addPassthroughCopy("./pretty-atom-feed.xsl");
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom",
+    outputPath: "/feed/feed.xml",
+    stylesheet: "/pretty-atom-feed.xsl",
+    collection: {
+      name: "posts",
+      limit: 10,
+    },
+    metadata: {
+      language: "en",
+      title: "Phil Parsons",
+      subtitle: "Web developer and bike enthusiast from London, UK",
+      base: "https://philparsons.co.uk/",
+      author: {
+        name: "Phil Parsons",
+      },
+    },
+  });
 }
